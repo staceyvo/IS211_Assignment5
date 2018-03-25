6c6d323314d381efc3e2c6f53c6b78b7b4b4a28e
staceyvo <staceyvo@gmail.com>
2018-03-24 22:32:17 -0400

    added comments to assignment 5

--- simulation.py
+++ simulation.py
@@ -1,73 +1,94 @@
 import argparse
 import csv
-import datetime
-import urllib2
-
-
-class Server:
-    def __init__(self, ppm):
-        self.page_rate = ppm
-        self.current_task = None
-        self.time_remaining = 0
-
-    def tick(self):
-        if self.current_task != None:
-            self.time_remaining = self.time_remaining - 1
-            if self.time_remaining <= 0:
-                self.current_task = None
-
-
-    def busy(self):
-        if self.current_task != None:
-            return True
-        else:
-        return False
-
-
-    def start_next(self, new_task):
-            self.current_task = new_task
-            self.time_remaining = new_task.get_pages() * 60 /
-            self.page_rate
-
 
 class Request:
+    """A class that represents a generic Request."""
+
     def __init__(self, arrival, filepath, duration):
+        """Constructor for the Request class.
+
+              Args:
+                  arrival (int): second the request arrives
+                  filepath (str): the file path of the request
+                  duration (int): time it takes to process
+
+              Attributes:
+                  arrival (int): second the request arrives
+                  filepath (str): the file path of the request
+                  duration (int): time it takes to process
+              """
         self.filepath = filepath
         self.duration = duration
         self.arrival = arrival
 
-    #TODO work it on out, ya dig
-    def measure_wait_time(self, current_time):
-        self.wait_time = current_time - self.timestamp
 
+def read_file(filename):
+    """This function, read_file will read our file and process it into a dictionary.
 
-def simulateOneServer(filename):
+                            Args:
+                                filename is the file we are reading
+
+                            Returns:
+                                Returns our data in a dictionary
+
+                    """
     requests = []
     with open(filename, 'r') as handle:
         reader = csv.DictReader(handle, fieldnames=['arrival', 'filepath', 'duration'])
         for row in reader:
-            requests.append(Request(row['arrival'], row['filepath'], row['duration']))
+            requests.append(Request(int(row['arrival']), row['filepath'], int(row['duration'])))
 
-    requests.sort(key=lambda x: x['arrival'])
-    requests[0]['completion'] = requests[0]['duration'] - requests[0]['arrival']
+    requests.sort(key=lambda x: x.arrival)
+    return requests
 
+def simulateOneServer(requests):
+    """This function, simulateOneServer will calculate the average latency for requests.
 
+                            Args:
+                                requests holds all requests to the server
 
-    process_time = requests[0]['duration']
+                            Returns:
+                                Returns the process_time
+
+                    """
+    requests[0].completion = requests[0].duration + requests[0].arrival
+
+    process_time = requests[0].duration
     for index in range(1, len(requests)):
         index_item = requests[index]
         prior_item = requests[index - 1]
 
-        index_item['completion'] = prior_item['completion'] + index_item['duration']
-        process_time += index_item['duration'] - index_item['arrival']
+        if prior_item.completion >= index_item.arrival:
+            index_item.completion = prior_item.completion + index_item.duration
+        else:
+            index_item.completion = index_item.arrival + index_item.duration
+        process_time += index_item.completion - index_item.arrival
 
     return process_time / float(len(requests))
-        
 
 
 
-def simulateManyServers():
-    raise Exception('Not yet implemented')
+#distribute requests to each server, then calculate avg latency for each
+def simulateManyServers(requests, servers):
+    """This function, simulateManyServers will process many requests at once,
+            distributing requests in a round robin fashion
+
+                            Args:
+                                requests holds all requests to the server
+                                servers is the number of servers given
+
+                            Returns:
+                                average latency for each server
+
+
+                    """
+    #divide requests into the same number as servers
+    result = []
+    for index in range(servers):
+        result.append(requests[index::servers])
+
+    #call simulateOneserver on each list
+    return [simulateOneServer(element) for element in result]
 
 
 if __name__ == '__main__':
@@ -75,11 +96,14 @@ if __name__ == '__main__':
     #setting up argparse
     parser = argparse.ArgumentParser(description='request simulator')
     parser.add_argument('--filename', help='File Name', required=True)
-    args = vars(parser.parse_args('filename'))
+    parser.add_argument('--servers',type=int, help='Number of Servers', required=False)
+    args = vars(parser.parse_args())
 
-    filename = args['filename']
+    requests = read_file(args['filename'])
 
-    simulateOneServer(filename)
-
-    #we will add an additional function to test against
-    simulateManyServers()
+    if 'servers' in args:
+        simulateManyServers(requests, args['servers'])
+    else:
+        print(simulateOneServer(requests))
+    print simulateOneServer(requests)
+    print simulateManyServers(requests, 10)
